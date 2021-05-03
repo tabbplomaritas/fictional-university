4cb5da25fdd988e86442c3bdc915d1b28bb10731
tabbplomaritas <tplomari@umich.edu>
2021-05-03 14:53:25 -0400

    updated plugins

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,23 @@
 <?php 
 
 function university_post_types(){
+// Campus Post Type
+register_post_type('event', array(
+        'supports' => array('title', 'editor', 'excerpt'),
+    'rewrite' => array('slug' => 'events'), // archive
+    'has_archive' => true,
+            'public' => true,
+    'labels' => array(
+      'name' => 'Events',
+      'add_new_item' => 'Add New Event',
+      'edit_item' => 'Edit Event',
+      'all_items' => 'All Events',
+      'singular_name' => 'Event'
+      ),
+    'menu_icon' => 'dashicons-calendar-alt' 
+  ));
+
+
   // Event Post Type
   register_post_type('event', array(
     'supports' => array('title', 'editor', 'excerpt'),
